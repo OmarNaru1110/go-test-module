ca792b6ddcd58cf349439824fad9827d3504a7bf
Naru <omarnaru2002@gmail.com>
2025-06-14 11:18:13 +0300

    new math package added

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/OmarNaru1110/go-test-module
+
+go 1.24.3
---
+++ math/math.go
@@ -0,0 +1,14 @@
+package math
+
+func Double(x int) int {
+	return x * 2
+}	
+func Triple(x int) int {
+	return x * 3
+}
+func Quadruple(x int) int {
+	return x * 4
+}
+func Square(x int) int {
+	return x * x
+}
